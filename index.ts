8a0cfbd7adffaa66cdd6265a91fce08b1d440f3d
pradoscript <prado.joao06480@gmail.com>
2024-12-02 16:21:35 -0300

    todo-list

---
+++ index.ts
@@ -0,0 +1,61 @@
+const form = document.querySelector("form")
+const input = document.querySelector("input")
+const section = document.querySelector("section")
+const ul = document.querySelector("ul")
+
+function removeButton(listWichWillBeRemoved) {
+
+  const button = document.createElement("button")
+  button.addEventListener("click", (ev) => {
+    ev.preventDefault()
+    ul.removeChild(listWichWillBeRemoved)
+  })
+  button.classList.add("removeAndEditClass")
+  button.innerText = "REMOVE"
+  
+  return button
+}
+
+function editButton(listWichWillBeEdited){
+  const button = document.createElement("button")
+  button.addEventListener("click", (ev) => {
+    if(input.value !== ""){
+      alert("The word space needs to be empty!")
+      return
+    }
+    ev.preventDefault()
+    input.value = listWichWillBeEdited.querySelector("span").innerText
+    ul.removeChild(listWichWillBeEdited)
+  })
+  button.classList.add("removeAndEditClass")
+  button.innerText = "EDIT"
+  
+  return button
+}
+
+function createTask() {
+  const text = input.value
+  const li = document.createElement("li")
+  const btnContainer = document.createElement("article")
+  btnContainer.classList.add("button-container")
+  const removeBtn = removeButton(li)
+  const editBtn = editButton(li)
+  removeBtn.classList.add("button-container")
+  editBtn.classList.add("button-container")
+  const taskText = document.createElement("span");
+  taskText.innerText = text;
+  btnContainer.append(removeBtn, editBtn)
+  li.append(taskText, btnContainer);
+  ul.appendChild(li);
+  input.value = "";
+}
+
+form.addEventListener("submit", (ev) => {
+  ev.preventDefault()
+  if (input.value == "") {
+    alert("You need to add a task!")
+    return
+  } 
+  createTask()
+})
+
